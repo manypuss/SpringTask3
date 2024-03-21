f598cc6f7b850f33039929a68729b285be80912b
”DeNitro” <denitro@gmail.com>
2024-03-21 21:33:26 +0300

    solution part 1

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,14 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Needle7 {
+
+    @Autowired
+    private Deth8 deth8;
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth8;
     }
 }
